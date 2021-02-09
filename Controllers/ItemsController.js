2d924e677ea2cf702b8c606335a870f69e069192
Maytham mahdi <metham553@gmail.com>
2021-02-09 16:40:39 +0300

    add update and delete item api

--- Controllers/ItemsController.js
+++ Controllers/ItemsController.js
@@ -1,20 +1,46 @@
 const Item = require('./../Models/ItemModel')
 
-exports.addItem = async (req, res) => {
-    const item =  new Item({
+exports.AddItem = async (req, res) => {
+    const item = new Item({
         name: req.body.name,
         type: req.body.type,
         image: req.body.image,
         isActive: req.body.isActive
     })
 
+
     try {
         const savedItem = await item.save()
-        res.json({savedItem: savedItem})
+        res.json({data: savedItem})
     } catch (e) {
-        res.json({error: e})
+        res.send({error: e})
     }
 }
 
+exports.UpdateItem = async (req, res) => {
+
+    const newItem = req.body;
+    try {
+        const item = await Item.findById(req.params.itemId);
+        console.log(item)
+        Object.assign(item, newItem)
+        const updatedItem = await item.save();
+        res.send({data: updatedItem})
+    } catch (e) {
+        res.send({error: e})
+    }
+}
+
+
+exports.DeleteItem = async (req, res) => {
+
+    try {
+        await Item.remove({_id: req.params.itemId})
+        res.send("The item was removed")
+
+    } catch (e) {
+        res.send({error: e})
+    }
+}
 
 
--- Routes/ItemRoutes.js
+++ Routes/ItemRoutes.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const ItemController = require('../Controllers/ItemsController')
 
-router.post('/AddItem', ItemController.addItem);
+router.post('/AddItem', ItemController.AddItem);
+router.patch('/EditItem/:itemId', ItemController.UpdateItem);
+router.delete('/DeleteItem/:itemId', ItemController.DeleteItem);
 
 
 module.exports = router
